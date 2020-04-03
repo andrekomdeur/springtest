b2b038d9fe27e182d1376cb7cf68d2de400cd6b1
andre <andre.komdeur@gmail.com>
2020-04-03 12:58:44 +0200

    Initial commit

---
+++ src/test/java/be/vdab/springtest/SpringtestApplicationTests.java
@@ -0,0 +1,9 @@
+package be.vdab.springtest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class SpringtestApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
